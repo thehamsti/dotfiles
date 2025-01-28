0a9803c5c412f2514538a636f83adba93dfd60d0
thehamsti <john@imp.gg>
2025-01-27 23:43:33 -0700

    Add aliases to the zshrc file

--- macos/setup_zsh.sh
+++ macos/setup_zsh.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 ## Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## Setup .zshrc by sourcing aliases.sh in it
+echo "source ~/dotfiles/macos/aliases.sh" >>~/.zshrc
