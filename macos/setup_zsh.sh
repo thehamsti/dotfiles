312f0b2c058cda7a29192e7786bbfc332533d844
John Cummings <john.cummings@k16solutions.com>
2025-01-28 12:33:49 -0700

    Check if alias is already in .zshrc before adding it

--- macos/setup_zsh.sh
+++ macos/setup_zsh.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+
 ## Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ## Setup .zshrc by sourcing aliases.sh in it
-echo "source ~/dotfiles/macos/aliases.sh" >>~/.zshrc
+ALIASES_LINE="source ~/dotfiles/macos/aliases.sh"
+grep -qxF "$ALIASES_LINE" ~/.zshrc || echo "$ALIASES_LINE" >>~/.zshrc
